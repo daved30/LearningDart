47006ed2811d72e96b692290161327a4ae8b7144
daved30 <mjfandev@gmail.com>
2022-07-06 21:39:46 +0530

    My first commit

---
+++ Main.dart
@@ -0,0 +1,9 @@
+import "dart:io";
+
+void main() {
+  print("enter name");
+  String? name = stdin.readLineSync();
+  print("Name is $name");
+  print("This is by som");
+  print("this is by dave");
+}
