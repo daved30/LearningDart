09ebf30467750628ad4eacfb980ade6b5466a1ba
daved30 <mjfandev@gmail.com>
2022-07-07 15:21:18 +0530

    madlibs using functions

--- Main.dart
+++ Main.dart
@@ -1,7 +1,16 @@
-void hi(String name, int age) {
-  print("Hi! $name welcome! you are $age years old! ");
+import "dart:io";
+
+String prompt(String prompt) {
+  print("$prompt : ");
+  String ans = stdin.readLineSync()!;
+  return ans;
 }
 
 void main() {
-  hi("Dave", 22);
+  String col = prompt("Color");
+  String noun = prompt("Noun");
+  String cel = prompt("Celebrity");
+  print("Roses are $col");
+  print("$noun are blue");
+  print("I Love $cel");
 }
