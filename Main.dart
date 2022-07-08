90e629f5b60ed331591e89ca9c3f0322a24ada12
daved30 <mjfandev@gmail.com>
2022-07-08 22:01:54 +0530

    classes and object using example mini game

--- Main.dart
+++ Main.dart
@@ -1,16 +1,32 @@
-import "dart:io";
+class Pokemon {
+  String name;
+  int level, hp, dmg;
+  Pokemon(this.name, this.level, this.hp, this.dmg);
+  void attack(Pokemon o, Pokemon p) {
+    print("${o.name} attack!");
+    print("${p.name} HP ==> ");
+    print(p.hp -= o.dmg);
+  }
 
-String prompt(String prompt) {
-  print("$prompt : ");
-  String ans = stdin.readLineSync()!;
-  return ans;
+  int getHP() {
+    return this.hp;
+  }
 }
 
 void main() {
-  String col = prompt("Color");
-  String noun = prompt("Noun");
-  String cel = prompt("Celebrity");
-  print("Roses are $col");
-  print("$noun are blue");
-  print("I Love $cel");
+  Pokemon p1 = new Pokemon("pika", 10, 100, 25);
+  Pokemon p2 = new Pokemon("evie", 10, 150, 5);
+  while (p1.getHP() >= 0 || p2.getHP() >= 0) {
+    if (p1.getHP() == 0 || p2.getHP() == 0) {
+      if (p1.getHP() == 0) {
+        print("${p2.name} is the winner!");
+      } else if (p2.getHP() == 0) {
+        print("${p1.name} is the winner!");
+      }
+      break;
+    }
+    p1.attack(p1, p2);
+    p2.attack(p2, p1);
+    print("==================================");
+  }
 }
